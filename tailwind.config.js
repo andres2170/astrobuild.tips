73e391c1094bc514b1b597be8496ec1775080d9e
Andres <epuentespuente@gmail.com>
2024-01-09 21:44:24 -0500

    cambio de data interna del archivo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*/*.{astro,html,js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+    }
   },
-  plugins: [require('@tailwindcss/line-clamp')],
-};
+  plugins: [require('@tailwindcss/line-clamp')]
+}
